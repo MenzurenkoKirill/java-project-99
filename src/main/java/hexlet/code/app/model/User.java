b55fc2d85dff5068538ba643773e3d75777b9217
Kirill Menzurenko <menzurenko1998@gmail.com>
2024-04-21 17:08:24 +0300

    Fix class User

--- src/main/java/hexlet/code/app/model/User.java
+++ src/main/java/hexlet/code/app/model/User.java
@@ -17,8 +17,12 @@ import lombok.ToString.Include;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Collection;
 
 @Entity
 @Getter
@@ -27,7 +31,7 @@ import java.time.LocalDate;
 @EntityListeners(AuditingEntityListener.class)
 @ToString(onlyExplicitlyIncluded = true)
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
-public class User implements BaseEntity {
+public class User implements UserDetails, BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Include
@@ -54,7 +58,37 @@ public class User implements BaseEntity {
     @LastModifiedDate
     private LocalDate updatedAt;
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return new ArrayList<GrantedAuthority>();
+    }
+
     public String getPassword() {
         return passwordDigest;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
     }
 }
